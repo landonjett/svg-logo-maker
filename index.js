b3599606b1cead5602cce84859814061fa467904
landonjett <landonjett@gmail.com>
2024-01-18 21:12:24 -0600

    added shape stest and async function

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Triangle, Circle, Square } = require('./lib/shapes');
+
+async function createLogo() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters for the logo:',
+            validate: input => input.length <= 3
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter the text color (keyword or hex):'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape:',
+            choices: ['Circle', 'Triangle', 'Square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter the shape color (keyword or hex):'
+        }
+    ]);
+
+}
